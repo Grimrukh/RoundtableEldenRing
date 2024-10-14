a3242d0a239b183e388093a683f31f30eb8d169f
Scott Mooney <swj.mooney@gmail.com>
2024-10-14 23:00:46 +1000

    Preparing for SMs

--- Dependencies/SoulsFormats/Util/IOodleCompressor.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace SoulsFormats;
-
-public interface IOodleCompressor
-{
-    public byte[] Compress(Span<byte> source);
-    public Memory<byte> Decompress(Span<byte> source, long uncompressedSize);
-}
